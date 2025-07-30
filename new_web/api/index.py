753b72ddfc9728728251ac8159faa2352b5e7dfa
S Sanjai <sanjai123@example.com>
2025-07-30 19:24:41 +0530

    Add or update new_web folder

--- new_web/api/index.py
+++ new_web/api/index.py
@@ -37,4 +37,5 @@ def remove_bg():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "true").lower() == "true"
+    print("==== Starting Flask server ====")
     app.run(host='0.0.0.0', port=port, debug=debug)
